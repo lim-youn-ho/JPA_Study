f888b92330cd7b7753f4d6a0a83c00e0754659ac
lim-youn-ho <limyounhoo@gmail.com>
2024-05-12 18:15:33 +0900

    -지점 정보 업데이트

--- src/main/java/com/example/jpa_study/controller/HouseController.java
+++ src/main/java/com/example/jpa_study/controller/HouseController.java
@@ -5,6 +5,7 @@ import com.example.jpa_study.dto.UserDTO;
 import com.example.jpa_study.entity.House;
 import com.example.jpa_study.entity.User;
 import com.example.jpa_study.repository.HouseRepository;
+import com.example.jpa_study.repository.UserRepository;
 import com.example.jpa_study.service.HouseUpdateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class HouseController {
 
+    private final HouseRepository houseRepository;
     @Autowired
     private final HouseUpdateService houseService;
 
@@ -21,6 +23,15 @@ public class HouseController {
     public void houseSave(@RequestBody HouseDTO house) {
         houseService.registerHouse(house);
 
+    }
+
+    @PutMapping("/house/{id}")
+    public void houseUpdate(@PathVariable Long id, @RequestBody House requestHouse) {
+        House house = houseRepository.findById(id).orElseThrow(() -> {
+            throw new IllegalArgumentException("해당하는 지점 아이디가 없습니다 id : " + id);
+        });
+
+        houseRepository.save(requestHouse);
     }
 
 
